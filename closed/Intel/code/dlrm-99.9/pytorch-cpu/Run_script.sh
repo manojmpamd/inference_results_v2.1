5fae4615c9852f92136a37869c5d892f39ea9a7e
manojmpamd <105292200+manojmpamd@users.noreply.github.com>
2023-10-26 11:04:13 +0530

    Update Run_script.sh

--- closed/Intel/code/dlrm-99.9/pytorch-cpu/Run_script.sh
+++ closed/Intel/code/dlrm-99.9/pytorch-cpu/Run_script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-cd /home/ML_DLRM/dlrm_pytorch
+cd /opt/workdir/code/dlrm-99.9/pytorch-cpu
 source ./setup_dataset.sh
 source ./setup_env_offline.sh
 bash run_mlperf.sh --mode=offline --type=perf --dtype=fp32
